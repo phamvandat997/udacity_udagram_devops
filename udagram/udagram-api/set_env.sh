4354c55ddd114f7b3a7d69f8a39f7da4b07652da
phamvandat997 <phamvandat0029@gmail.com>
2024-11-09 16:44:03 +0700

    Change config

--- udagram/udagram-api/set_env.sh
+++ udagram/udagram-api/set_env.sh
@@ -1,8 +1,8 @@
-export POSTGRES_USERNAME=postgres
-export POSTGRES_PASSWORD=postgres
-export POSTGRES_HOST=database.cst5cbmcpryb.us-east-1.rds.amazonaws.com
-export POSTGRES_DB=postgres
-export AWS_BUCKET=udagramweb
-export AWS_REGION=us-east-1
-export AWS_PROFILE=default
-export JWT_SECRET=CFKuJaSr+0k3tWmdztDWNMJG70MoN5DZCKO6O3u+
+export POSTGRES_USERNAME='postgres'
+export POSTGRES_PASSWORD='postgres'
+export POSTGRES_HOST='database.cst5cbmcpryb.us-east-1.rds.amazonaws.com'
+export POSTGRES_DB='postgres'
+export AWS_BUCKET='udagramweb'
+export AWS_REGION='us-east-1'
+export AWS_PROFILE='default'
+export JWT_SECRET='CFKuJaSr+0k3tWmdztDWNMJG70MoN5DZCKO6O3u+'
